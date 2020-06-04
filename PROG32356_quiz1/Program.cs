17e76b7db7278f5b5896aed98eb56753485bd670
Manuel Sanchez <manuel.sanchezm@gmail.com>
2020-06-03 21:51:29 -0400

    My own code idea...

--- PROG32356_quiz1/Program.cs
+++ PROG32356_quiz1/Program.cs
@@ -11,13 +11,63 @@ namespace InheritanceApp
     {
         static List<Employee> employees = new List<Employee>();
 
-        static void Main(string[] args)
+        static void Main()
         {
+            #region Manuel's Code
+            /*
+            Console.WriteLine("Enter one of following types of Employees: " + "(Accountant / Mechanic / Pilot)\n");
+            string employeeType = Console.ReadLine();
 
-            MockData();
+            if (!string.IsNullOrEmpty(employeeType))
+            {
+                Employee employee = null;
+
+                long number = 1;
+                string firstName = "John";
+                string lastName = "Doe";
+                string type = employeeType;
+
+                switch (employeeType)
+                {
+                    case "Accountant":
+                        string accTitle = "Clerk";
+                        string licenseType = "Audit License";
+                        employee = new Accountant(number, firstName, lastName
+                            , type, accTitle, licenseType);
+                        break;
+                    case "Mechanic":
+                        string title = "title 1";
+                        string certification = "certif 1";
+                        employee = new Mechanic(number, firstName, lastName
+                            , type, title, certification);
+                        break;
+                    case "Pilot":
+                        string license = "PRO";
+                        string ratings = "100";
+                        employee = new Mechanic(number, firstName, lastName
+                            , type, license, ratings);
+                        break;
+                    default:
+                        Console.WriteLine($"{employeeType} is not valid employee.");
+                        break;
+                }
+
+                if (employee != null)
+                {
+                    Console.WriteLine($"{ employee.PrintEmployee()}");
+                    Thread.Sleep(3000);
+                }
+
+            }
+            */
+            #endregion
 
             Console.WriteLine("----- EMPLOYEES LIST APPLICATION -----");
             Console.WriteLine("Enter the letter to visualize the employees per category: ");
+
+            MockData();
+
+
             // We do more stuff here
 
 
