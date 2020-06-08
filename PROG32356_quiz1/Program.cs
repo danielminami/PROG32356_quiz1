bb2d6a24c0744133c67be368170f77bed473666e
Manuel Sanchez <manuel.sanchezm@gmail.com>
2020-06-08 16:37:34 -0400

    .

--- PROG32356_quiz1/Program.cs
+++ PROG32356_quiz1/Program.cs
@@ -1,45 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace InheritanceApp
 {
     class Program
     {
-        static List<Employee> employees = new List<Employee>();
+        static List<Employee> employees = null;
 
         static void Main(string[] args)
         {
-
             MockData();
 
             Console.WriteLine("----- EMPLOYEES LIST APPLICATION -----");
-            Console.WriteLine("Enter the letter to visualize the employees per category: ");
-            // We do more stuff here
 
+            do
+            {
+                Console.Clear();
 
-            foreach (Employee emp in employees) {
-                if (emp is Mechanic) {
-                    Mechanic m = (Mechanic)emp;
-                    Console.WriteLine(m.PrintEmployee());
+                Console.WriteLine("Enter one of the following employees typing " +
+                    "only the capital letter\n" +
+                    "(A)ccountant / (M)echanic / (P)ilot): ");
+                string employeeType = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(employeeType) &&
+                    (employeeType.Equals("A") || employeeType.Equals("M") || employeeType.Equals("P")))
+                {
+                    foreach (Employee emp in employees)
+                    {
+                        if (employeeType.Equals("M") && emp is Mechanic)
+                        {
+                            Console.WriteLine($"{emp.PrintEmployee()}");
+                        }
+                        else if (employeeType.Equals("P") && emp is Pilot)
+                        {
+                            Console.WriteLine($"{emp.PrintEmployee()}");
+                        }
+                        else if (employeeType.Equals("A") && emp is Accountant)
+                        {
+                            Console.WriteLine($"{emp.PrintEmployee()}");
+                        }
+                    }
                 }
-            }
+                else
+                {
+                    Console.WriteLine($"\nYou have entered an invalid employee.");
+                }
+
+                Thread.Sleep(4000);
+
+                Console.WriteLine("\nEnter \"y\" to make another operation or any to exit.");
+            } while (Console.ReadLine() == "y");
 
             Thread.Sleep(4000);
-
         }
 
-        static void MockData() {
-            Mechanic m1 = new Mechanic(298301, "Robert", "DeNiro", "Full Time", "Automobile Mechanic", "BMW Certification");
-
-            employees.Add(m1);
-
-
+        static void MockData()
+        {//should i change the names here ??? 
+            employees = new List<Employee>() {
+                new Accountant(298301, "Robert", "DeNiro", "Full Time", "Clerk", "Audit License"),
+                new Mechanic(298302, "Paul", "Sams", "Part Time", "Automobile Mechanic", "BMW Certification"),
+                new Pilot(298303, "Craig", "Flyer", "Contractor", "B88POK09", "Boeing Certified")
+            };
         }
-
     }
 }
-
